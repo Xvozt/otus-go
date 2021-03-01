4ca4ef60187c8fb5f09c62ae19a682a5f4cfe6fb
Sergey Kupriyanov <sv.kupriyanov@icloud.com>
2021-03-01 19:04:36 +0300

    add unpacker function and test

--- hw2/unpacker/unpacker.go
+++ hw2/unpacker/unpacker.go
@@ -1,10 +1,30 @@
 package unpacker
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
 
 func Unpack(s string) (string, error) {
 	if len(s) == 0 {
 		return "", fmt.Errorf("empty string")
 	}
-	return s, nil
+
+	r := []rune(s)
+	var result = strings.Builder{}
+
+	for i := 0; i < len(r); i++ {
+		if n, err := strconv.Atoi(string(r[i])); err == nil {
+			if i == 0 || (i > 0 && unicode.IsDigit(r[i-1])) {
+				return "", fmt.Errorf("invalid string")
+			}
+			result.WriteString(strings.Repeat(string(r[i-1]), n-1))
+		} else {
+			result.WriteRune(r[i])
+		}
+	}
+
+	return result.String(), nil
 }
--- hw2/unpacker/unpacker_test.go
+++ hw2/unpacker/unpacker_test.go
@@ -5,9 +5,18 @@ import (
 	"testing"
 )
 
-func TestUnpack_Without_Escape(t *testing.T) {
-	actual := "abcd"
-	expected := "abcd"
+func TestUnpack_Without_Escape_Two(t *testing.T) {
+	actual, _ := Unpack("a4bc2d5e")
+	expected := "aaaabccddddde"
 
 	assert.Equal(t, expected, actual)
+}
+
+func TestUnpack_Empty_String(t *testing.T) {
+	actual, err := Unpack("")
+	expectedErrorMsg := "empty string"
+	expected := ""
+
+	assert.EqualErrorf(t, err, expectedErrorMsg, "Error should be: %v, got: %v", expectedErrorMsg, err)
+	assert.Equal(t, actual, expected)
 }
